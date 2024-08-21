2cccd7ac38d99cfeb7ce7e8c4db5d007f80a35b8
SRK-Focusteck <shahrukh.khan@focusteck.com>
2024-08-21 17:37:06 +0500

    making 2nd last commit with personal account and change to own account

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ let a =12
 let b=13
 console.log(a+b+123)
 
-const funA =()=> console.log("are we on right track")
+const funA = () => console.log("are we on right track")
+const funB = () => console.log("are we on right track")
